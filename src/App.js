c294538546a68a25630845f3e1b4db2f8a23a5de
Anurag pandey <52875857+anurag99x@users.noreply.github.com>
2021-12-20 10:30:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import logo from './logo.svg';
 import "./App.css"
 import Home from "../src/Components/Home";
-import data from "./data.json";
+import MainData from "./data.json";
 import Searchbar from './Components/Search';
 
-let index = 0;
-data.map((e)=>{
-    e.push(index++)       
+let i = 0;
+MainData.map((e)=>{
+    e.push(i++)       
 });
-//console.log(data)
-
 function App() {
   
   return (
     <div className="main_div">
-      {/* <Searchbar data1={data}/> */}
-      <Home data={data}/>
+      <Home data={MainData}/>
     </div>
   );
 }
